99361b541046a288bdda01fd40dfcf3e164fe8ae
Vladimir Kruglov <kruglovvladimir723@gmail.com>
2024-11-10 15:37:43 +0300

    Рабочий MVP
    Удалено все лишнее
    Добавлены основные файлы:
    основной - task_scheduler.py
    вспомогательный - task_utils.py

---
+++ task_scheduler.py
@@ -0,0 +1,57 @@
+from task_utils import (load_json_data, build_station_dict, build_graph,
+                        remove_unconnected_nodes, build_labels_dict, find_station_id,
+                        Task, assign_initial_tasks, assign_remaining_tasks)
+
+from datetime import timedelta, time
+from operator import itemgetter
+
+def main():
+    # Загрузка данных для графа
+    names = load_json_data("l10n.json")
+    graph_data = load_json_data("data.json")
+
+    # Построение графа и словарей
+    node_station_dict = build_station_dict(graph_data['stops']['items'])
+    global G, labels  # Делаем G и labels доступными в assign_tasks_to_employees
+    G = build_graph(graph_data['nodes']['items'], graph_data['links']['items'])
+    G = remove_unconnected_nodes(G)
+    labels = build_labels_dict(names, node_station_dict)
+
+    # Пример списка задач
+    tasks_list = [["Ботанический сад", "Рижская", 9, 30], ["ВДНХ", "Проспект мира", 10, 45], ["Охотный ряд", "Театральная", 11, 15], ["Новокосино", "Шоссе Энтузиастов", 12, 0], ["Киевская", "Арбатская", 13, 20], ["Беговая", "Улица 1905 года", 14, 50], ["Речной вокзал", "Войковская", 15, 10], ["Южная", "Чертановская", 16, 30], ["Таганская", "Китай-город", 17, 5], ["Перово", "Новогиреево", 17, 45], ["Щёлковская", "Первомайская", 18, 15], ["Третьяковская", "Парк Культуры", 19, 0], ["Бабушкинская", "Свиблово", 20, 25], ["Лубянка", "Чистые пруды", 21, 40], ["Каширская", "Коломенская", 22, 30], ["Бабушкинская", "Таганская", 8, 45], ["Тропарёво", "Славянский бульвар", 9, 20], ["Юго-Западная", "Киевская", 10, 0], ["Площадь Ильича", "Каширская", 11, 30], ["Щукинская", "Парк Победы", 12, 15], ["Полежаевская", "Октябрьская", 13, 45]]
+
+    # Сортировка заявок по времени начала
+    tasks_list = sorted(tasks_list, key=itemgetter(2, 3))
+
+    # Инициализация задач с проверкой наличия станций
+    task_objects = []
+    for t in tasks_list:
+        st1_id = find_station_id(labels, t[0])
+        st2_id = find_station_id(labels, t[1])
+
+        if st1_id is None or st2_id is None:
+            print(f"Предупреждение: Одна или обе станции '{t[0]}' или '{t[1]}' не найдены в графе. Заявка пропущена.")
+            continue  # Пропустить задание, если одна из станций не найдена
+
+        # Добавляем задачу только если станции найдены
+        task_objects.append(Task(t[0], t[1], time(t[2], t[3]), G, labels))
+
+    # Количество сотрудников
+    num_employees = 3
+    employees = [{'id': i + 1, 'available_time': timedelta(0), 'last_station': None} for i in range(num_employees)]
+
+    # Распределение начальных заявок
+    assign_initial_tasks(task_objects, employees)
+
+    # Распределение оставшихся заявок
+    assign_remaining_tasks(task_objects, employees, G, labels)
+
+    # Вывод информации о распределении заявок
+    for task in task_objects:
+        print(f"Заявка {task.id}: Сотрудник {task.employee_id}, {task.start_station} -> {task.end_station}, "
+              f"Начало: {task.start_time}, Окончание: {task.end_time}")
+
+
+# Запуск основной функции
+if __name__ == "__main__":
+    main()
---
+++ task_utils.py
@@ -0,0 +1,117 @@
+import json
+import networkx as nx
+import codecs
+from datetime import timedelta
+
+
+def load_json_data(file_path):
+    """Загружает JSON-данные из указанного файла."""
+    with codecs.open(file_path, "r", "utf_8_sig") as file:
+        return json.load(file)
+
+
+def build_station_dict(stops):
+    """Создает словарь соответствия узлов (nodeId) и станций (stationId)."""
+    return {stop['nodeId']: stop['stationId'] for stop in stops}
+
+
+def build_graph(nodes, links):
+    """Создает граф на основе узлов и связей с учетом времени на каждой связи."""
+    G = nx.Graph()
+    for node in nodes:
+        G.add_node(node['id'])
+    for link in links:
+        G.add_edge(link['fromNodeId'], link['toNodeId'], length=link['attributes']['time'])
+    return G
+
+
+def remove_unconnected_nodes(G):
+    """Удаляет узлы с числом связей менее 2 для упрощения графа."""
+    nodes_to_remove = [node for node in G.nodes() if len(G.edges(node)) < 2]
+    G.remove_nodes_from(nodes_to_remove)
+    return G
+
+
+def build_labels_dict(names, node_station_dict):
+    """Создает словарь названий станций по идентификаторам узлов."""
+    labels = {}
+    for node, station_id in node_station_dict.items():
+        labels[node] = names['keysets']['generated'].get(station_id + '-name', {}).get('ru', 'error')
+    return labels
+
+
+def calc_time(G, st1, st2):
+    """Рассчитывает время в пути между двумя станциями по их идентификаторам."""
+    try:
+        return round(nx.dijkstra_path_length(G, source=st1, target=st2, weight="length") / 60)
+    except nx.NetworkXNoPath:
+        return None
+
+
+def find_station_id(labels, station_name):
+    """Находит идентификатор узла по названию станции."""
+    for node_id, name in labels.items():
+        if name.lower() == station_name.lower():
+            return node_id
+    return None
+
+
+class Task:
+    """Класс для представления задачи."""
+    task_counter = 1
+
+    def __init__(self, ss, es, st, G, labels):
+        self.id = Task.task_counter
+        self.start_station = ss
+        self.end_station = es
+        self.start_time = timedelta(hours=st.hour, minutes=st.minute)
+        self.end_time = self.calculate_end_time(G, labels)
+        self.employee_id = None
+        Task.task_counter += 1
+
+    def calculate_end_time(self, G, labels):
+        """Вычисляет время завершения задачи на основе графа и времени пути между станциями."""
+        st1_id = find_station_id(labels, self.start_station)
+        st2_id = find_station_id(labels, self.end_station)
+        duration_minutes = calc_time(G, st1_id, st2_id)
+
+        if duration_minutes is not None:
+            return self.start_time + timedelta(minutes=duration_minutes)
+        else:
+            return self.start_time
+
+
+def assign_initial_tasks(tasks, employees):
+    """Назначает начальную задачу каждому сотруднику."""
+    for i, task in enumerate(tasks[:len(employees)]):
+        task.employee_id = employees[i]['id']
+        employees[i]['available_time'] = task.end_time
+        employees[i]['last_station'] = task.end_station
+
+
+def find_best_employee_for_task(task, employees, G, labels):
+    """Находит сотрудника, который быстрее всех сможет добраться до станции отправления задачи."""
+    min_travel_time = timedelta.max
+    best_employee = None
+
+    for employee in employees:
+        travel_time = timedelta(minutes=calc_time(G, find_station_id(labels, employee['last_station']),
+                                                  find_station_id(labels, task.start_station)))
+        arrival_time = employee['available_time'] + travel_time
+
+        if arrival_time <= task.start_time and travel_time < min_travel_time:
+            min_travel_time = travel_time
+            best_employee = employee
+
+    return best_employee
+
+
+def assign_remaining_tasks(tasks, employees, G, labels):
+    """Распределяет оставшиеся задачи, выбирая сотрудника с минимальным временем прибытия на место."""
+    for task in tasks[len(employees):]:
+        best_employee = find_best_employee_for_task(task, employees, G, labels)
+
+        if best_employee:
+            task.employee_id = best_employee['id']
+            best_employee['available_time'] = task.end_time
+
